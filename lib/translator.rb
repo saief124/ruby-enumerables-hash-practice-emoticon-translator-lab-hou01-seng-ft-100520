47bf129c18d7ac7d7e636b8f58df89c03d497c90
Saief I Sayed <saiefsayed123@hotmail.com>
2020-09-24 17:27:17 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -22,6 +22,7 @@ def get_japanese_emoticon(file_path=".lib/emoticons.yml",emoticons)
   sorry_message="Sorry, that emoticon was not found"
 
  b.each{|k,v|key_identifier=v}
+ key_identifier[:japanese]
  binding.pry
    if key_identifier[:japanese]==emoticons
      return key_identifier[:english]
