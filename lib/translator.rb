d8cc75e5cea6e42153bbabafdd7b2a0ad97849df
Saief I Sayed <saiefsayed123@hotmail.com>
2020-09-24 19:07:30 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -26,8 +26,6 @@ def get_japanese_emoticon(file_path=".lib/emoticons.yml",emoticons)
       if b[k][:english]==emoticons
       return b[k][:japanese]
 
-    elsif b[k][:english]!=emoticons
-      return sorry_message
  end
 end
  #
