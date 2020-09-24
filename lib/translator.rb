755f8a521437fe73b09ee0c1db1c63c132282d51
Saief I Sayed <saiefsayed123@hotmail.com>
2020-09-24 16:35:00 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,12 +1,53 @@
-# require modules here
+require "yaml"# require modules here
+require "pry"
 
-def load_library
-  # code goes here
-end
+def load_library(file_location)
+#hash={:english=>"", :japanese=>""} # initialize a new hash
+YAML.load_file(file_location) #loading file returns a hash, need to iterate over this hash and assign values to english and japanese
 
-def get_japanese_emoticon
+  YAML.load_file(file_location).each_with_object({}) do |(k, v), a|
+
+    a[k]={}
+    a[k][:english]=v[0]
+    a[k][:japanese]=v[1]
+
+    end # ends do
+end #ends method
+
+def get_japanese_emoticon(file_path=".lib/emoticons.yml",emoticons)
   # code goes here
-end
+  b={}
+  key_identifier=""
+  b=load_library(file_path)
+  sorry_message="Sorry, that emoticon was not found"
+  #b["angel"]=>{:english=>"O:)", :japanese=>"Japanesesymbol"}
+  #b["angel"][:english]=>"O:)"
+  #from the value received from argument emoticons identify the key and return the other value in that key
+  #b.each_with_object({}) do |(k,v),a|
+  #  a[k]={}
+
+
+        b.fetch(:japanese)
+
+      binding.pry
+
+
+
+
+
+
+
+  #  else
+  #    sorry_message
+   #end
+
+
+#    if v[:japanese]==emoticons
+#    v[:english]
+#   } #ends if..else
+ #ends Enumerables
+#binding.pry
+end #ends method
 
 def get_english_meaning
   # code goes here
